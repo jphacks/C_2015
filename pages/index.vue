7c4c11518450726fa04ef6c34362b02b83f7dc36
flat35hd99 <flat35hd99@gmail.com>
2020-11-03 19:18:34 +0900

    add failureStory logic

--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,31 @@
         content="hogehoge"
       />
     </v-col>
+    {{ failures }}
   </v-row>
 </template>
 
 <script>
 import FailureCard from '@/components/failure/FailureCard.vue'
+import { API } from 'aws-amplify'
+import { listFailures } from '~/graphql/queries'
 
 export default {
   components: {
     FailureCard
+  },
+  data () {
+    return {
+      failures: []
+    }
+  },
+  methods: {
+    async getFailures () {
+      const failures = await API.graphql({
+        query: listFailures
+      })
+      this.failures = failures.data.listFailures.items
+    }
   }
 }
 </script>
