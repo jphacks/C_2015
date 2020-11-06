a28821b20e1a55f11ef879e12b394678296ea540
flat35hd99 <flat35hd99@gmail.com>
2020-11-06 20:37:03 +0900

    change schema

--- components/webpush/AssignWebpush.vue
+++ components/webpush/AssignWebpush.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <v-btn
+      v-if="!isPushNotificationsEnabled"
       @click="activateWebpush"
     >
       プッシュ通知を有効化する
@@ -9,8 +10,18 @@
 </template>
 
 <script>
+import { API } from 'aws-amplify'
+import { createOnesingle } from '~/graphql/mutations'
 export default {
   name: 'AssingWebpush',
+  created () {
+    this.checkisAlreadySubscribed()
+  },
+  data () {
+    return {
+      isPushNotificationsEnabled: false
+    }
+  },
   methods: {
     activateWebpush () {
       this.$OneSignal.push(() => {
@@ -25,8 +36,20 @@ export default {
         this.$OneSignal.showNativePrompt()
       })
     },
-    createOnesingle (id) {
-      // ここにOnesingleモデルにデータ突っ込む処理。
+    checkisAlreadySubscribed () {
+      this.$OneSignal.push(() => {
+        this.$OneSignal.isPushNotificationsEnabled((isEnabled) => {
+          this.isPushNotificationsEnabled = isEnabled
+        })
+      })
+    },
+    async createOnesingle (userId) {
+      await API.graphql({
+        query: createOnesingle,
+        variables: { input: {
+          onesingle: userId
+        } }
+      })
     }
   }
 }
