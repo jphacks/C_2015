fae7cc7d3b99a484f4138dbc2343e34cb63bb5e6
澁谷悠大 <univer405@gmail.com>
2020-11-07 12:13:32 +0900

    failurecard btn action

--- components/failure/FailureCard.vue
+++ components/failure/FailureCard.vue
@@ -19,23 +19,27 @@
           <v-card-text>
             名言が<span>{{ sayingsCount }}</span>コ送られました！
           </v-card-text>
-          <v-btn @click="showSayings">名言一覧</v-btn>
-          <v-btn @click="createSaying">名言を送る</v-btn>
-          <v-btn @click="toggleMetoo" text>
-            <v-icon>
-              mdi-heart-outline
-            </v-icon>
-          </v-btn>
-          <v-btn @click="toggleSorry" text>
-            <v-icon>
-              mdi-coffee-outline
-            </v-icon>
-          </v-btn>
-          <v-btn @click="toggleNice" text>
-            <v-icon>
-              mdi-thumb-up-outline
-            </v-icon>
-          </v-btn>
+          <div>
+            <v-btn @click="showSayings">名言一覧</v-btn>
+            <v-btn @click="createSaying">名言を送る</v-btn>
+          </div>
+          <div class="responses">
+            <v-btn class="response-btn" @click="toggleMetoo" text>
+              <v-icon>
+                mdi-heart-outline
+              </v-icon>
+            </v-btn>
+            <v-btn class="response-btn" @click="toggleSorry" text>
+              <v-icon>
+                mdi-coffee-outline
+              </v-icon>
+            </v-btn>
+            <v-btn class="response-btn" @click="toggleNice" text>
+              <v-icon>
+                mdi-thumb-up-outline
+              </v-icon>
+            </v-btn>
+          </div>
         </v-card-actions>
       </v-col>
     </v-row>
@@ -108,6 +112,8 @@ export default {
   margin-right: 30%;
 }
 .actions{
+  padding: 0 1rem;
+  justify-content: space-around;
 }
 .username{
   height: 100%;
@@ -122,6 +128,12 @@ export default {
   padding: .5rem 20%;
 }
 .v-btn{
+  box-shadow: none;
+  background-color: #9D1D22 !important;
+  color: white;
+  border-radius: 0;
+}
+.v-btn.response-btn{
   box-shadow: none;
   background-color: #9D1D22 !important;
   color: white;
